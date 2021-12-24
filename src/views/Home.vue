704bb79030830ebdd9a90e6854a85456812cb871
joseluisgs <joseluisgonsan@gmail.com>
2021-12-24 13:27:34 +0100

    Maquetación Desktop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,35 +8,37 @@
       </h1>
       <!-- Add Idea -->
       <section class="mb-6">
-        <form>
+        <!-- aplico flex a partir de sm: 640, asi son los puntos de ruptura -->
+        <form class="sm:flex">
           <input
             type="text"
             required
-            class="w-full p-3"
+            class="w-full p-3
+              sm:flex-auto"
             placeholder="Add your idea"
           >
           <input
             type="submit"
             value="Add idea"
-            class="w-full p-2 bg-gray-600 text-white"
+            class="w-full p-2 bg-gray-600 text-white sm:flex-1"
           >
         </form>
       </section>
       <!-- Ideas -->
-      <article class="p-3 rounded-lg bg-gray-300">
+      <article class="p-3 rounded-lg bg-gray-300 sm:(flex items-center)">
         <!-- Info -->
-        <section class="text-center">
-          <h2 class="text-xl">
+        <section class="text-center sm:(flex-1 text-left)">
+          <h2 class="text-xl sm:(leading-6 text-2xl)">
             Esto es una idea lorem ipsum dolor sit amet.
           </h2>
           <small>Username</small>
         </section>
         <!-- Votos -->
-        <section class="pt-3 border-t-2 mt-6 border-black">
+        <section class="pt-3 border-t-2 mt-6 border-black sm:(pt-0 pl-3 border-t-0 border-l-2 mt-0 flex items-center)">
           <h3 class="text-3xl font-bold text-center">
             99
           </h3>
-          <nav class="flex justify-center">
+          <nav class="flex justify-center sm:(block pl-2)">
             <img
               class="w-10 cursor-pointer"
               src="../assets/images/arrow.svg"
@@ -58,7 +60,6 @@
 </template>
 
 <script>
-
 export default {
   name: 'Home'
 }
