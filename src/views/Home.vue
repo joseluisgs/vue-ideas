de5503aefa7be2822b045d8d41411924691a7a4b
joseluisgs <joseluisgonsan@gmail.com>
2021-12-24 21:35:12 +0100

    Auth

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,11 +18,32 @@
             placeholder="Add your idea"
           >
           <input
+            v-if="user"
             type="submit"
             value="Add idea"
             class="w-full p-2 bg-gray-600 text-white sm:flex-1"
           >
         </form>
+        <p
+          v-if="!user"
+          class="mt-2 text-center"
+        >
+          Please <a
+            class="text-blue-500 font-bold underline"
+            href="#"
+            @click="doLogin"
+          > do login </a> to add an idea.
+        </p>
+        <p
+          v-else
+          class="mt-2 text-center"
+        >
+          Hi 👋 {{ user.displayName }}. <a
+            class="text-green-600 font-bold underline"
+            href="#"
+            @click="doLogout"
+          > Logout </a>.
+        </p>
       </section>
       <!-- Ideas -->
       <AppIdea
@@ -40,6 +61,7 @@
 <script>
 import AppIdea from '../components/AppIdea.vue'
 import data from '../services/data.json'
+import Firebase from '../services/Firebase'
 import { ref } from 'vue'
 export default {
   name: 'Home',
@@ -49,12 +71,35 @@ export default {
 
   setup () {
     const ideas = ref(data.ideas)
+    const user = ref(null)
+    // Me conecto a firebase y obtengo el usuario actual
+    Firebase.auth.onAuthStateChanged(async auth => (user.value = auth || null))
+    // login
+    const doLogin = async () => {
+      try {
+        await Firebase.auth.signInWithPopup(Firebase.providerGoogle)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    // logout
+    const doLogout = async () => {
+      try {
+        await Firebase.auth.signOut()
+      } catch (error) {
+        console.error(error)
+      }
+    }
     return {
-      ideas
+      ideas,
+      user,
+      doLogin,
+      doLogout
     }
   }
 }
 </script>
 
 <style scoped>
+
 </style>
