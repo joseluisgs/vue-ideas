c6410e8b1a5b247c29bf16073db215bea42304ce
joseluisgs <joseluisgonsan@gmail.com>
2021-12-24 12:07:43 +0100

    Firebase Setup

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import Service from './services/Firebase'
 
 import 'virtual:windi.css'
 import 'virtual:windi-devtools'
 
 import './assets/styles/base.css'
+
+// Firebase
+if (import.meta.env.VITE_APP_STAGE === 'dev') {
+  console.log(`⚑ Firebase -> ${Service.defaultProject.name} ✓`)
+}
 
 createApp(App)
   .use(router)
