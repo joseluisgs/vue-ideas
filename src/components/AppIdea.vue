87fb9893e8424b1a0c95dd05e931a6e41cbff6d9
joseluisgs <joseluisgonsan@gmail.com>
2021-12-24 13:34:18 +0100

    Componentes AppIdea

---
+++ src/components/AppIdea.vue
@@ -0,0 +1,39 @@
+<template>
+  <article class="p-3 rounded-lg bg-gray-300 sm:(flex items-center)">
+    <!-- Info -->
+    <section class="text-center sm:(flex-1 text-left)">
+      <h2 class="text-xl sm:(leading-6 text-2xl)">
+        Esto es una idea lorem ipsum dolor sit amet.
+      </h2>
+      <small>Username</small>
+    </section>
+    <!-- Votos -->
+    <section class="pt-3 border-t-2 mt-6 border-black sm:(pt-0 pl-3 border-t-0 border-l-2 mt-0 flex items-center)">
+      <h3 class="text-3xl font-bold text-center">
+        99
+      </h3>
+      <nav class="flex justify-center sm:(block pl-2)">
+        <img
+          class="w-10 cursor-pointer"
+          src="../assets/images/arrow.svg"
+          alt="Vote up"
+        >
+        <img
+          class="w-10 cursor-pointer transform -rotate-180"
+          src="../assets/images/arrow.svg"
+          alt="Vote down"
+        >
+      </nav>
+    </section>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'AppIdea'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,33 +25,7 @@
         </form>
       </section>
       <!-- Ideas -->
-      <article class="p-3 rounded-lg bg-gray-300 sm:(flex items-center)">
-        <!-- Info -->
-        <section class="text-center sm:(flex-1 text-left)">
-          <h2 class="text-xl sm:(leading-6 text-2xl)">
-            Esto es una idea lorem ipsum dolor sit amet.
-          </h2>
-          <small>Username</small>
-        </section>
-        <!-- Votos -->
-        <section class="pt-3 border-t-2 mt-6 border-black sm:(pt-0 pl-3 border-t-0 border-l-2 mt-0 flex items-center)">
-          <h3 class="text-3xl font-bold text-center">
-            99
-          </h3>
-          <nav class="flex justify-center sm:(block pl-2)">
-            <img
-              class="w-10 cursor-pointer"
-              src="../assets/images/arrow.svg"
-              alt="Vote up"
-            >
-            <img
-              class="w-10 cursor-pointer transform -rotate-180"
-              src="../assets/images/arrow.svg"
-              alt="Vote down"
-            >
-          </nav>
-        </section>
-      </article>
+      <AppIdea />
       <!-- /Idea -->
     </div>
     <!-- /Main Box -->
@@ -60,8 +34,12 @@
 </template>
 
 <script>
+import AppIdea from '../components/AppIdea.vue'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    AppIdea
+  }
 }
 </script>
 
